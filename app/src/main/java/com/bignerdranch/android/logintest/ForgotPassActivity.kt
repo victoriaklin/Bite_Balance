3941611dc12391537a295fac13fe40cfd0a17d8c
victorialin <vicki@bu.edu>
2023-11-29 13:40:01 -0500

    UI

---
+++ app/src/main/java/com/bignerdranch/android/logintest/ForgotPassActivity.kt
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.logintest
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class ForgotPassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_pass)
+
+        val sendEmailButton = findViewById<Button>(R.id.send_button)
+        sendEmailButton.setOnClickListener {
+            // if email is in database show toast saying "A link to reset your password has been sent."
+            // if email not in database show toast saying "That email is not associated with an account"
+        }
+    }
+}
