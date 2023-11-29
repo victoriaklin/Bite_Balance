4e20b36155c975d55e101968c61eeb73d3788459
victorialin <vicki@bu.edu>
2023-11-29 14:27:29 -0500

    UI

--- app/src/main/java/com/bignerdranch/android/logintest/HomeActivity.kt
+++ app/src/main/java/com/bignerdranch/android/logintest/HomeActivity.kt
@@ -2,8 +2,11 @@ package com.bignerdranch.android.logintest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
