8eb138667b50a57c411251388ab782e827390fdc
victorialin <vicki@bu.edu>
2023-11-29 14:53:55 -0500

    Added searchView + recyclerView and "Generate Recipes" button

--- app/src/main/java/com/bignerdranch/android/logintest/HomeActivity.kt
+++ app/src/main/java/com/bignerdranch/android/logintest/HomeActivity.kt
@@ -3,12 +3,29 @@ package com.bignerdranch.android.logintest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchView: SearchView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        searchView = findViewById(R.id.searchView)
+
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        addDataToList()
+    }
+
+    private fun addDataToList(){
+
     }
 }
